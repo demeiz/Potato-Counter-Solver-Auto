cf1d0149e48526b987a6147753b2734210f90bd0
demeiz <medievalduel@gmail.com>
2015-05-20 06:56:23 +0100

    d

---
+++ Neopets Potato Counter Solver Auto/Neopets_Potato_Counter_Solver_Auto.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name        Potato Counter Solver - Auto
+// @namespace   Jarofgrease.captainmaxthecat.com
+// @description Solver for the potato counter. It's auto baby!
+// @author 		Demeiz
+// @email 		admin@captainmaxthecat.com
+// @homepage	http://www.captainmaxthecat.com
+// @version		1.0
+// @language	en
+// @include		*.neopets.com/medieval/potatocounter.*
+// @grant		none
+// ==/UserScript==
+
+// Okay so it's not pretty, but this is an effective way to count the potatoes. Previously the value was accessible via the source.
+value=document.evaluate("count(/html/body/div/div[3]/table/tbody/tr/td[2]/table/tbody/tr/td/img)",document,null,1,null).numberValue
+// The guess should be in the box now..So we'll click the button
+Button = document.evaluate("//input[@value='Guess!']", document, null,7, null)
+buttonclick = Button.snapshotItem(0)
+buttonclick.click()
+// Well if we're here then it's worked so far. Last bit of code restarts it ^__^
+second = document.evaluate("//input[@value='Play Again']", document, null,7, null)
+clickretry = second.snapshotItem(0)
+clickretry.click()
